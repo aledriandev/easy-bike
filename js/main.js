1098cb6eed8b201c02487a521e181ec17cb0ad30
AQP20171679 <alejandraadriantejada@gmail.com>
2017-09-11 08:08:05 -0500

    funcion buscarcompleta

--- js/main.js
+++ js/main.js
@@ -37,20 +37,55 @@ function initMap(){
 /**/
 const easyBike = {
 
-	mapa: {
-		zoom: undefined,
-		center: undefined
+	mapas: {
+		mapaInicial: undefined,
+		miUbicacion: undefined
+	},
+
+	posicionActual : {
+		latitud: undefined,
+		longitud: undefined
 	},
 
 	iniciarMapa : function() {
-		var mapaInicial = new google.maps.Map(document.getElementById('map'),{
+		easyBike.mapas.mapaInicial = new google.maps.Map(document.getElementById('map'),{
 			zoom: 5,
 			center: { lat: -9.1191427, lng: -77.0349046},
 			mapTypeControl: false,
 			zoomControl: false,
 			streetViewControl:false
 		});
+		easyBike.funciones();
 	},
 
+	funciones : function(){
+		document.getElementById('encuentrame').addEventListener('click',easyBike.buscar);
+	},
+
+	buscar : function(){
+		if(navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(easyBike.funcionExito,easyBike.funcionError);
+		}
+	},
+
+	funcionExito : function(posicion){
+		easyBike.posicionActual.latitud = posicion.coords.latitude;
+		easyBike.posicionActual.longitud = posicion.coords.longitude;
+
+		easyBike.mapas.miUbicacion = new google.maps.Marker({
+			position:{lat:easyBike.posicionActual.latitud, lng:easyBike.posicionActual.longitud},
+			animation: google.maps.Animation.DROP,
+			map:easyBike.mapas.mapaInicial
+		});
+
+		easyBike.mapas.mapaInicial.setZoom(17);
+		easyBike.mapas.mapaInicial.setCenter({lat:easyBike.posicionActual.latitud, lng:easyBike.posicionActual.longitud});
+	},
+
+	funcionError : function(error){
+		alert('Tenemos un problema con encuentrar tu ubicacion');
+	}
+
 };
+
 $(document).ready(easyBike.iniciarMapa);
